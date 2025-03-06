6e3551d7f48a9f73e865654606e6e4c241818133
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-03-05 21:07:40 -0800

    Add NotFound Page

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,7 @@ import ProfileSettings from "../pages/ProfileSettings";
 import ReaderDetails from "../pages/ReaderDetails";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import NotFound from "../pages/NotFound";
 
 const AppRoutes = () => {
   return (
@@ -17,6 +18,7 @@ const AppRoutes = () => {
         <Route path="/reader/:readerId" element={<ReaderDetails />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
