4d752563c41312e4455f04453e2c66dbf407b680
Fabricio <briciolg13@gmail.com>
2025-03-05 21:44:15 -0800

    working bookDetails page

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -1,12 +1,18 @@
-import { Component } from "react";
+import React,{ Component } from "react";
+import {Container, Row} from "react"
 
 class BookDetails extends Component {
   state = {};
+  
   render() {
     return (
-      <>
+      <div>
         <h1>Book Details</h1>
-      </>
+        <Row className = "my-4">
+          <h1>teste</h1>
+        </Row>
+      </div>
+     
     );
   }
 }
