e24038d14364c20ee65278e604ef1bf7504cc56d
William Delmanto <67394462+WRDelmanto@users.noreply.github.com>
2025-03-09 22:29:43 -0700

    Merge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
+import PostCard from "../components/PostCard";
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
+import { fetchPosts, fetchReaderById } from "../services/MockAPI";
 import PostCard from "../components/PostCard";
 import { fetchPosts, fetchReaderById } from "../services/MockAPI";
 
 const Home = () => {
+	const [posts, setposts] = useState([]);
+	const [filteredposts, setFilteredposts] = useState([]);
+	const [account_reader, setReader] = useState();
+	const ACCOUNT_READER_ID = "754368128"; // For testing purposes
 	const [account_reader, setReader] = useState([]);
 	const [posts, setposts] = useState([]);
 	const [filteredposts, setFilteredposts] = useState([]);
 	const ACCOUNT_READER_ID = "754368128"; // For testing purposes
 
 	useEffect(() => {
+		const getReader = async () => {
+			const account_reader = await fetchReaderById(ACCOUNT_READER_ID);
+			setReader(account_reader);
+
+			// console.log("Account reader:", account_reader);
+		};
+
+		const getposts = async () => {
+			const fetchedposts = await fetchPosts();
+			setposts(fetchedposts);
+			setFilteredposts(fetchedposts);
 		const getReader = async () => {
 			const account_reader = await fetchReaderById(ACCOUNT_READER_ID);
 			setReader(account_reader);
@@ -24,9 +41,11 @@ const Home = () => {
 			setposts(fetchedposts);
 			setFilteredposts(fetchedposts);
 
+			// console.log("posts:", fetchedposts);
 			console.log("posts:", fetchedposts);
 		};
 
+		getReader().then(getposts);
 		getReader().then(getposts);
 	}, []);
 
@@ -36,10 +55,14 @@ const Home = () => {
 
 		if (searchInput === "") {
 			setFilteredposts(posts);
+			setFilteredposts(posts);
 		} else {
 			const filtered = posts.filter((post) =>
 				post.book.title.toLowerCase().includes(searchInput)
+			const filtered = posts.filter((post) =>
+				post.book.title.toLowerCase().includes(searchInput)
 			);
+			setFilteredposts(filtered);
 			setFilteredposts(filtered);
 		}
 	};
@@ -49,6 +72,15 @@ const Home = () => {
 	};
 
 	const handleFavorite = (bookID) => {
+		if (account_reader.favoriteBooks.includes(bookID)) {
+			const index = account_reader.favoriteBooks.indexOf(bookID);
+			account_reader.favoriteBooks.splice(index, 1);
+		} else {
+			account_reader.favoriteBooks.push(bookID);
+		}
+
+		setReader({ ...account_reader });
+		console.log("Updated bookId: " + bookID + " to favorite: " + account_reader.favoriteBooks.includes(bookID));
 		if (account_reader.favoriteBooks.includes(bookID)) {
 			const index = account_reader.favoriteBooks.indexOf(bookID);
 			account_reader.favoriteBooks.splice(index, 1);
@@ -70,9 +102,27 @@ const Home = () => {
 
 		setReader({ ...account_reader });
 		console.log("Updated bookId: " + bookID + " to bookmarked: " + account_reader.bookmarkedBooks.includes(bookID));
+		if (account_reader.bookmarkedBooks.includes(bookID)) {
+			const index = account_reader.bookmarkedBooks.indexOf(bookID);
+			account_reader.bookmarkedBooks.splice(index, 1);
+		} else {
+			account_reader.bookmarkedBooks.push(bookID);
+		}
+
+		setReader({ ...account_reader });
+		console.log("Updated bookId: " + bookID + " to bookmarked: " + account_reader.bookmarkedBooks.includes(bookID));
 	};
 
 	const handleCompleted = (bookID) => {
+		if (account_reader.completedBooks.includes(bookID)) {
+			const index = account_reader.completedBooks.indexOf(bookID);
+			account_reader.completedBooks.splice(index, 1);
+		} else {
+			account_reader.completedBooks.push(bookID);
+		}
+
+		setReader({ ...account_reader });
+		console.log("Updated bookId: " + bookID + " to completed: " + account_reader.completedBooks.includes(bookID));
 		if (account_reader.completedBooks.includes(bookID)) {
 			const index = account_reader.completedBooks.indexOf(bookID);
 			account_reader.completedBooks.splice(index, 1);
@@ -92,6 +142,13 @@ const Home = () => {
 			{/* Body */}
 			<Container style={{ marginTop: "64px" }}>
 				<Row className="g-4">
+					{filteredposts.map((post) => (
+						<PostCard
+							key={post._Id}
+							book={post.book}
+							isFavorite={account_reader.favoriteBooks.includes(post.book.bookId)}
+							isBookmarked={account_reader.bookmarkedBooks.includes(post.book.bookId)}
+							isCompleted={account_reader.completedBooks.includes(post.book.bookId)}
 					{filteredposts.map((post) => (
 						<PostCard
 							key={post._Id}
