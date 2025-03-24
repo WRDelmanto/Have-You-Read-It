09cd1fc67cae10ae46d47b6fc841f16a8384a293
Fabricio <briciolg13@gmail.com>
2025-03-23 22:41:27 -0700

    AuthorDetails new version

--- src/pages/AuthorDetails.jsx
+++ src/pages/AuthorDetails.jsx
@@ -13,7 +13,8 @@ const AuthorDetails = () => {
   const [isFollowing, setIsFollowing] = useState(false);
   const [posts, setPosts] = useState([]);
   const [filteredPosts, setFilteredPosts] = useState([]);
-  const [visibleCount, setVisibleCount] = useState(6);
+  const [visibleCount, setVisibleCount] = useState(4);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getAuthor = async () => {
@@ -156,8 +157,8 @@ const AuthorDetails = () => {
                   </p>
                 </div> */}
             {/* Author Books */}
-            <Row>
-              <Col className="text-start">
+            {/* <Row> */}
+            {/* <Col className="text-start">
                 <div
                   className="mt-4 p-4"
                   style={{
@@ -195,10 +196,10 @@ const AuthorDetails = () => {
                         </Col>
                       </Row>
                     </Card>
-                  ))}
+                  ))}*/}
 
-                  {/* Button"Read More" */}
-                  {visibleCount < books.length && (
+            {/* Button"Read More" */}
+            {/* {visibleCount < books.length && (
                     <div className="text-center mt-3">
                       <Button
                         variant="info"
@@ -207,17 +208,84 @@ const AuthorDetails = () => {
                         Read More
                       </Button>
                     </div>
-                  )}
+                  )} */}
 
-                  {/* <h5 className="fw-bold">Posts about this author</h5>
+            {/* <h5 className="fw-bold">Posts about this author</h5>
                   {filteredPosts.length > 0 &&
                     filteredPosts.map((post) => (
                       <PostCard key={post._id} post={post} />
                     ))} */}
-                  {/* <p>"show list of bookswritten by this author "</p> */}
-                </div>
-              </Col>
-            </Row>
+            {/* <p>"show list of bookswritten by this author "</p> */}
+            {/* </div> */}
+            <div
+              className="mt-4 p-4"
+              style={{ borderTop: "1px solid #dcdcdc", paddingTop: "10px" }}
+            >
+              <h5 className="fw-bold">Author's Books {`(${books.length})`}</h5>
+              <Card className="shadow-sm p-3">
+                <Row className="mt-3">
+                  {visibleBooks.map((book) => (
+                    <Col key={book.bookId} md={3} className="mb-3 d-flex">
+                      <Card className="shadow-sm w-100 border-0">
+                        <Card.Img
+                          variant="top"
+                          src={book.cover || "https://via.placeholder.com/150"}
+                          alt={book.title}
+                          style={{
+                            height: "250px",
+                            width: "100%",
+                            objectFit: "cover",
+                            cursor: "pointer",
+                          }}
+                          onClick={() => navigate(`/book/${book.bookId}`)}
+                          onMouseEnter={(e) =>
+                            (e.currentTarget.style.transform = "scale(1.05)")
+                          }
+                          onMouseLeave={(e) =>
+                            (e.currentTarget.style.transform = "scale(1)")
+                          }
+                        />
+                        <Card.Body className="text-center">
+                          <Card.Title
+                            className="small"
+                            style={{
+                              display: "-webkit-box",
+                              WebkitBoxOrient: "vertical",
+
+                              maxHeight: "3em",
+                            }}
+                            onClick={() => navigate(`/book/${book.bookId}`)}
+                            onMouseEnter={(e) =>
+                              (e.currentTarget.style.transform = "scale(1.05)")
+                            }
+                            onMouseLeave={(e) =>
+                              (e.currentTarget.style.transform = "scale(1)")
+                            }
+                          >
+                            {book.title}
+                          </Card.Title>
+                          <Card.Text className="text-muted small">
+                            {/* {author?.authorName || "Unknown"} */}
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ))}
+                </Row>
+
+                {/* Read More Button */}
+                {visibleCount < books.length && (
+                  <div className="text-center mt-3">
+                    <Button
+                      variant="info"
+                      onClick={() => setVisibleCount(visibleCount + 4)}
+                    >
+                      Read More
+                    </Button>
+                  </div>
+                )}
+              </Card>
+            </div>
           </Card>
         </Container>
       )}
